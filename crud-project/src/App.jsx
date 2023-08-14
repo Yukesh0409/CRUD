588820b0c776840b9afbbe333090b20090423176
Yukesh0409 <Yukesh0409>
2023-08-14 20:54:30 +0530

    First

---
+++ crud-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import{BrowserRouter, Routes,Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './Users'
+import UpdateUser from './UpdateUser'
+import CreateUser from './CreateUser'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users/>}></Route>
+          <Route path='/create' element={<CreateUser/>}></Route>
+          <Route path='/update/:id' element={<UpdateUser/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
